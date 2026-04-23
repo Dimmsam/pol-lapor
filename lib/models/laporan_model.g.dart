cb0985afcf8beefa590d3f04686cc97b478b3794
revanramdani31 <permanavean09@gmail.com>
2026-04-24 01:02:49 +0700

    menambahkan form perlaporan, kamera live dan juga menambahkan submit ke hive

--- lib/models/laporan_model.g.dart
+++ lib/models/laporan_model.g.dart
@@ -22,13 +22,19 @@ class LaporanLokalAdapter extends TypeAdapter<LaporanLokal> {
       deskripsi: fields[2] as String,
       status: fields[3] as int,
       isSynced: fields[4] as bool,
+      kategori: fields[5] as String?,
+      lokasi: fields[6] as String?,
+      nomorInventaris: fields[7] as String?,
+      tingkatKerusakan: fields[8] as String?,
+      fotoPath: fields[9] as String?,
+      createdAt: fields[10] as DateTime?,
     );
   }
 
   @override
   void write(BinaryWriter writer, LaporanLokal obj) {
     writer
-      ..writeByte(5)
+      ..writeByte(11)
       ..writeByte(0)
       ..write(obj.uuid)
       ..writeByte(1)
@@ -38,7 +44,19 @@ class LaporanLokalAdapter extends TypeAdapter<LaporanLokal> {
       ..writeByte(3)
       ..write(obj.status)
       ..writeByte(4)
-      ..write(obj.isSynced);
+      ..write(obj.isSynced)
+      ..writeByte(5)
+      ..write(obj.kategori)
+      ..writeByte(6)
+      ..write(obj.lokasi)
+      ..writeByte(7)
+      ..write(obj.nomorInventaris)
+      ..writeByte(8)
+      ..write(obj.tingkatKerusakan)
+      ..writeByte(9)
+      ..write(obj.fotoPath)
+      ..writeByte(10)
+      ..write(obj.createdAt);
   }
 
   @override
