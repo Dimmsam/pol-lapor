bd03b190ceb7db3ebcb1f4bee2124871adc2a116
revanramdani31 <permanavean09@gmail.com>
2026-04-24 01:02:49 +0700

    menambahkan form perlaporan, kamera live dan juga menambahkan submit ke hive

--- lib/models/laporan_model.dart
+++ lib/models/laporan_model.dart
@@ -19,11 +19,35 @@ class LaporanLokal extends HiveObject {
   @HiveField(4)
   bool isSynced;
 
+  @HiveField(5)
+  String? kategori;
+
+  @HiveField(6)
+  String? lokasi;
+
+  @HiveField(7)
+  String? nomorInventaris;
+
+  @HiveField(8)
+  String? tingkatKerusakan;
+
+  @HiveField(9)
+  String? fotoPath;
+
+  @HiveField(10)
+  DateTime createdAt;
+
   LaporanLokal({
     required this.uuid,
     required this.judul,
     required this.deskripsi,
     this.status = 1,
     this.isSynced = false,
-  });
+    this.kategori,
+    this.lokasi,
+    this.nomorInventaris,
+    this.tingkatKerusakan,
+    this.fotoPath,
+    DateTime? createdAt,
+  }) : createdAt = createdAt ?? DateTime.now();
 }
